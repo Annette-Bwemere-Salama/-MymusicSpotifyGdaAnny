bcf5cc80dc28987a278db710c6621b284bf109ca
annette Bwemere <bwemereannette@gmail.com>
2022-08-31 11:50:24 +0200

    refactory: arrangement de l'api probleme grave

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ const CLIENT_ID = "8eeb90f7753a45548dd9e8a19da17fd9";
 const CLIENT_SECRET = "665e67a536144aa9ab70cf954e51d307";
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState([]);
+  const [searchInput, setSearchInput] = useState("gims");
+  const [accessToken, setAccessToken] = useState({});
   const [album, setAlbum] = useState("");
 
   useEffect(() => {
@@ -14,56 +14,68 @@ function App() {
     let authParameters = {
       method: "POST",
       headers: {
-        "Content-Type": "application/k-www-form-urlencoded",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body:
-        "grant_type-client_credentials&client_id" +
+        "grant_type=client_credentials&client_id=" +
         CLIENT_ID +
         "&client_secret=" +
         CLIENT_SECRET,
     };
-    fetch("https://accounts.spotify.com.api/token", authParameters)
+    fetch("https://accounts.spotify.com/api/token", authParameters)
       .then((result) => result.json())
-      .then((data) => setAccessToken(data.accessToken));
+      .then((data) => {
+        console.log(data, accessToken?.access_token);
+        setAccessToken(data);
+        if (accessToken) {
+          search();
+        }
+      });
   }, []);
+
+  console.log(accessToken);
 
   //Search
 
-  async function search() {
+  function search() {
+    console.log(accessToken);
     console.log("Search for" + searchInput);
+
+    //obtenir une requête en utilisant la recherche pour obtenir l'identifiant de l'artiste
+    let artisteParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken?.access_token,
+      },
+    };
+    console.log(accessToken);
+    fetch(
+      "https://api.spotify.com/v1/search?q=" +
+        searchInput +
+        "&type=track,album",
+      // authParameters,
+      artisteParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    //obtenir une requête en utilisant la recherche par le nom de l'album de l'artiste
+    // let returndAlbums = fetch(
+    //   "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10" +
+    //        +
+    //     "/albums" +
+    //     "?include_groups=albumarket=US&limit=50",
+    //   artisteParameters
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setAlbum(data.items);
+    //   });
   }
 
-  //obtenir une requête en utilisant la recherche pour obtenir l'identifiant de l'artiste
-  let artisteParameters = {
-    method: "GET",
-    Headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer" + accessToken,
-    },
-  };
-  let artisteId = fetch(
-    "https://api.spotify.com/v1/search?q=" + searchInput + "&typesartist",
-    searchParameters,
-    artisteParameters
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      return data.artists.items[0].id;
-    });
-  console.log("Artist ID is" + artisteId);
-  //obtenir une requête en utilisant la recherche par le nom de l'album de l'artiste
-  let returndAlbums = fetch(
-    "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10" +
-      artisteId +
-      "/albums" +
-      "?include_groups=albumarket=US&limit=50",
-    searchParameters
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      setAlbum(data.items);
-    });
   //afficher ces albums à l'utilisateur
   return (
     <div className="App">
