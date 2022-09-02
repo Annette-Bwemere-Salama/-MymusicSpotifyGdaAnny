d4bf48e26ce89bd57b26adbfb32024fcb4de2b01
annette Bwemere <bwemereannette@gmail.com>
2022-09-02 10:06:00 +0200

    feat: resolution probleme github

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import { useEffect, useState } from "react";
 const CLIENT_ID = "8eeb90f7753a45548dd9e8a19da17fd9";
 const CLIENT_SECRET = "665e67a536144aa9ab70cf954e51d307";
 
+// function RetourRes() {
+//   return (
+
+//   );
+// }
+
 function App() {
   const [searchInput, setSearchInput] = useState("gims");
-  const [accessToken, setAccessToken] = useState({});
+  const [accessToken, setAccessToken] = useState();
   const [album, setAlbum] = useState("");
 
   useEffect(() => {
@@ -26,104 +32,85 @@ function App() {
       .then((result) => result.json())
       .then((data) => {
         console.log(data, accessToken?.access_token);
-        setAccessToken(accessToken.access_token);
-        if (accessToken) {
-          search();
-        }
+        setAccessToken(data.access_token);
+        console.log(accessToken?.access_token);
       });
   }, []);
+  useEffect(() => {
+    if (accessToken) search();
+  }, [accessToken]);
 
+  // search();
 
-
-  //Search
-
-  function search() {
-
-
+  async function search() {
+    console.log(accessToken.access_token);
     //obtenir une requête en utilisant la recherche pour obtenir l'identifiant de l'artiste
     let artisteParameters = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + accessToken?.access_token,
+        Authorization: "Bearer " + accessToken?.access_token,
       },
     };
-
-    fetch(
-      "https://api.spotify.com/v1/search?q=" +
-        searchInput +
-        "&type=track,album",
-      // authParameters,
+    let artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
       artisteParameters
     )
       .then((response) => response.json())
       .then((data) => {
-        // console.log(data);
+        console.log(data);
+        return data.artists && data.artists.items && data.artists.items[0].id;
       });
-    //obtenir une requête en utilisant la recherche par le nom de l'album de l'artiste
-    // let returndAlbums = fetch(
-    //   "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10" +
-    //        +
-    //     "/albums" +
-    //     "?include_groups=albumarket=US&limit=50",
-    //   artisteParameters
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     setAlbum(data.items);
-    //   });
-  }
 
-  //afficher ces albums à l'utilisateur
+    fetch(
+      `https://api.spotify.com/v1/search?q=${searchInput}
+        &type=artist&limit=20/n`,
+      artisteParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("l'artiste c'est " + artistID);
+    // obtenir une requête en utilisant la recherche par le nom de l'album de l'artiste
+    let returndAlbums = fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50" +
+        artisteParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbum(data.items);
+      });
+  }
   return (
     <div className="App">
       <h1>Mes préferer</h1>
-      <form>
-        <input
-          placeholder="Rechercher une chanson par nom ou par musicien"
-          className="monbutton"
-          onKeyPress={(event) => {
-            if (event.key == "Enter") {
-              search();
-            }
-          }}
-          onChange={(event) => setSearchInput(event.target.value)}
-        ></input>
+      <form
+        placeholder="Rechercher une chanson par nom ou par musicien"
+        className="monbutton"
+        onKeyPress={(event) => {
+          if (event.key == "Enter") {
+            search();
+          }
+        }}
+        onChange={(event) => setSearchInput(event.target.value)}
+      >
+        <input></input>
         <button onClick={search}>Sarch</button>
       </form>
       {/* <div className="menu">
-        {/* {album.map((album, i) => {
-          return (
-            <main
-              style={{
-                justifyContent: "center",
-                height: "150px",
-                width: "200px",
-              }}
-            >
-              <img src={album.images[0]} />
-              <h5>{album}</h5>
-            </main>
-          );
-        })} 
+        {album.map((album, i) => {
+          <main>
+            <img src={album.images[0]} />
+            <h5>{album}</h5>
+          </main>;
+        })}
       </div> */}
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        <main
-          style={{
-            justifyContent: "center",
-            height: "150px",
-            width: "200px",
-          }}
-        >
-          <img src="vite.svg" alt="photo" />
-          <h5>Nom de l'Album</h5>
-        </main>
-      </div>
     </div>
   );
 }
